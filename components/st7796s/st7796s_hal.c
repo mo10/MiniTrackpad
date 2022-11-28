6af0296bc116c2cfc9397356497d29a4702116b3
某10 <0w0@0w0.pw>
2022-11-28 22:18:32 +0800

    Update .gitignore

---
+++ components/st7796s/st7796s_hal.c
@@ -0,0 +1,17 @@
+#include "st7796s_hal.h"
+
+
+static int st7789s_8080_init() {
+
+    return 0;
+}
+
+static int st7789s_spi_init() {
+
+    return 0;
+}
+
+static int st7789s_rgb_init() {
+
+    return 0;
+}
---
+++ components/st7796s/st7796s_hal.h
@@ -0,0 +1,6 @@
+#ifndef ST7796S_HAL_H
+#define ST7796S_HAL_H
+
+
+
+#endif
